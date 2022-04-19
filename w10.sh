63ea7fc8d96cbc04b04fad092025d1f77d53f9cc
AlexBodas007 <103894209+AlexBodas007@users.noreply.github.com>
2022-04-20 03:18:41 +0800

    w10.sh

--- w10.sh
+++ w10.sh
@@ -1,36 +1,19 @@
-echo "===================================="
-echo "Download windows files"
-echo "===================================="
-curl -L -o w10x64.img https://bit.ly/akuhnetW10x64
-echo "===================================="
-echo "Download ngrok"
-echo "===================================="
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-unzip ngrok-stable-linux-amd64.zip &>/dev/null &
-unzip ngrok-stable-linux-amd64.zip1 &>/dev/null &
-./ngrok authtoken 281dyoqa2xckS5oLzesF1dDvHwf_5tEWyQjQSQae12SNqZyKN
-nohup ./ngrok tcp 3388 &>/dev/null &
-./ngrok tcp 3388 &>/dev/null &
-echo "===================================="
-echo Downloading File From akuh.net
-echo "===================================="
-apt-get install qemu > /dev/null 2>&1
-echo "===================================="
-echo "Wait"
-echo "Starting Windows"
-echo "===================================="
-echo "===================================="
-echo RDP Address:
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp 5900 &>/dev/null &
+echo Please wait for installing...
+sudo apt update -y > /dev/null 2>&1
+echo "Installing QEMU (2-3m)..."
+sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
+echo Downloading Windows Disk...
+curl -L -o lite10.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.1.3/providers/qemu.box
+echo "Windows 10 x64 Lite On Google Colab"
+echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "===================================="
-echo "===================================="
-echo "Ctrl+C To Copy"
-echo "Wait 1-2 minute to finish bot"
-echo "Dont Close This Tab"
-echo "===================================="
-echo "===================================="
-echo "Username: akuh"
-echo "Password: Akuh.Net"
-echo "===================================="
-qemu-system-x86_64 -hda w10x64.img -m 8G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic  > /dev/null 2>&1
-sleep 43200
+echo "Note: Use Right-Click Or Ctrl+C To Copy"
+echo "Please Keep Colab Tab Open, Maximum Time 12h"
+echo Script by fb.com/thuong.hai.581
+sudo qemu-system-x86_64 -vnc :0 -hda lite10.qcow2  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
+
