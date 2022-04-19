1e5f733100d5d8b4e34f21abc8abdf5bbc3dc3bb
AlexBodas007 <103894209+AlexBodas007@users.noreply.github.com>
2022-04-20 03:21:51 +0800

    w10.sh

--- w10.sh
+++ w10.sh
@@ -1,7 +1,6 @@
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
-./ngrok authtoken $CRP 
+./ngrok authtoken 281dyoqa2xckS5oLzesF1dDvHwf_5tEWyQjQSQae12SNqZyKN
 nohup ./ngrok tcp 5900 &>/dev/null &
 echo Please wait for installing...
 sudo apt update -y > /dev/null 2>&1
